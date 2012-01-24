1335562fbecc6b15817d55818bf290b4c407c218
Marc Jeanson <marc@redlinesoftware.com>
2012-01-24 09:25:31 -0600

    Fill in the steps with the code I wish I had

--- features/step_definitions/activities_steps.rb
+++ features/step_definitions/activities_steps.rb
@@ -1,12 +1,15 @@
-Given /^the following activities exist:$/ do |table|
-  # table is a Cucumber::Ast::Table
-  pending # express the regexp above with the code you wish you had
+Given /^the following activities exist:$/ do |activities|
+  @activities = activities.hashes.collect { |activity| Activity.create! activity }
 end
 
 When /^I'm on the activities page$/ do
-  pending # express the regexp above with the code you wish you had
+  visit activities_path
 end
 
 Then /^I should see all the activities$/ do
-  pending # express the regexp above with the code you wish you had
+  @activities.each do |activity|
+    within_table('activities') do
+      page.should have_content(activity.title)
+    end
+  end
 end
