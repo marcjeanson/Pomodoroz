8041e45338a59967bd6455159822495192cddd3d
Marc Jeanson <marc@redlinesoftware.com>
2012-01-24 09:52:57 -0600

    Adding a spec for the index action

--- spec/controllers/activities_controller_spec.rb
+++ spec/controllers/activities_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
 describe ActivitiesController do
+  context "get .index" do
+    before do
+      get :index
+    end
 
+    it { should respond_with(:success) }
+    it { should render_template(:index) }
+  end
 end
