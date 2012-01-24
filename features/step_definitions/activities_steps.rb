ff1daf6b08d327c748d913ca359641d00b3d3321
Marc Jeanson <marc@redlinesoftware.com>
2012-01-24 09:22:06 -0600

    adding the generated cucumber steps

---
+++ features/step_definitions/activities_steps.rb
@@ -0,0 +1,12 @@
+Given /^the following activities exist:$/ do |table|
+  # table is a Cucumber::Ast::Table
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I'm on the activities page$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see all the activities$/ do
+  pending # express the regexp above with the code you wish you had
+end
