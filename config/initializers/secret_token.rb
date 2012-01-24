f840e58ca398debbbbff9cc4d779965ee1d88586
Marc Jeanson <marc@redlinesoftware.com>
2012-01-23 23:14:05 -0600

    result of running: rails new pomodoroz -d postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pomodoroz::Application.config.secret_token = '64d6cd9ad9a20370b489404362c8c2dfc92fc6350bc65c6a813868cd10ba86f6bbe188df2842f7f0b2dfa8228b07c1f909169c42011d3dd6428267016cfca535'
