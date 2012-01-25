56a5b5092d1e971e48cfe1da87c2e1fa190839af
Marc Jeanson <marc@redlinesoftware.com>
2012-01-25 11:14:26 -0600

    adding some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+activity1 = Activity.create title: "Read requirements document"
+activity2 = Activity.create title: "Reinstall Visual Studio", completed: true
+activity3 = Activity.create title: "Migrate from TFS to Git"
+
+activity1.pomodoros.create completed_at: 1.day.ago
+activity1.pomodoros.create reset_at: 2.days.ago
+activity1.pomodoros.create completed_at: 3.days.ago
+
+activity2.pomodoros.create completed_at: 1.day.ago
+
+activity3.pomodoros.create completed_at: 1.day.ago
+activity3.pomodoros.create completed_at: 2.days.ago
+activity3.pomodoros.create completed_at: 4.days.ago
+activity3.pomodoros.create reset_at: 2.days.ago
