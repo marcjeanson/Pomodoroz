fc01115e5b9c42e23c9d9aa27b584a000358772d
Amir Barylko <amir@barylko.com>
2012-01-25 11:02:42 -0600

    Added completed and reset info from the show method

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -1,8 +1,14 @@
 class ChartsController < ApplicationController
 
   def show
-    # summary daily, weekly, monthly
-    Pomodoro.all
+    # summary last 7 days of finished vs reset
+    sda = Date.today - 6
+    completed = Pomodoro.where("completed_at >= '#{sda}'").group_by { |p| p.completed_at.to_date }  
+    reset = Pomodoro.where("reset_at >= '#{sda}'").group_by { |p| p.reset_at.to_date }  
+    
+    @dates = (sda..Date.today)
+    @completed = @dates.map { |d| completed[d].nil? ? 0 : completed[d].count }
+    @reset = @dates.map { |d| reset[d].nil? ? 0 : reset[d].count }
   end
 
 end
--- spec/controllers/charts_controller_spec.rb
+++ spec/controllers/charts_controller_spec.rb
@@ -2,11 +2,25 @@ require 'spec_helper'
 
 describe ChartsController do
 
-  describe "GET 'show'" do
-    it "returns http success" do
-      get 'show'
-      response.should be_success
+  describe "#show" do
+    let(:today) { Date.parse('2012-01-10') }
+    let(:sda)   { today - 6 }
+    let(:days)  { (sda..today) }
+    let(:completed) { days.map { |d| stub_model(Pomodoro, completed_at: d) } }
+    let(:reset) { days.map { |d| stub_model(Pomodoro, reset_at: d) } }
+
+    before do 
+      Date.stub(:today).and_return(today)
+      Pomodoro.stub(:where).with("completed_at >= '#{sda}'").and_return(completed)
+      Pomodoro.stub(:where).with("reset_at >= '#{sda}'").and_return(reset)
+      get :show
     end
+    
+    it { should respond_with(:success) }
+    it { should render_template(:show) }
+    it { should assign_to(:dates).with(days) }
+    it { should assign_to(:completed).with([1, 1, 1, 1, 1, 1, 1]) }
+    it { should assign_to(:reset).with([1, 1, 1, 1, 1, 1, 1]) }
   end
 
 end
